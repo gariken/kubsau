39c0379c550b33bc1bf8719b831a55050e10ad04
Alexander Goryatov <schet941@gmail.com>
2015-12-23 01:21:52 +0300

    alpha

--- KUbSAU/ViewController.swift
+++ KUbSAU/ViewController.swift
@@ -9,6 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var group: UIView!
+    @IBOutlet weak var prepod: UIView!
+    @IBOutlet weak var audioriya: UIView!
+    @IBOutlet weak var index: UISegmentedControl!
+    
+    
+    @IBAction func index(sender: AnyObject) {
+        switch index.selectedSegmentIndex
+            
+        {
+            
+        case 0:
+            
+            group.hidden = false
+            
+            prepod.hidden = true
+            
+            audioriya.hidden = true
+            
+        case 1:
+            
+            group.hidden = true
+            
+            prepod.hidden = false
+            
+            audioriya.hidden = true
+            
+        case 2:
+            
+            group.hidden = true
+            
+            prepod.hidden = true
+            
+            audioriya.hidden = false
+            
+        default:
+            
+            break;
+            
+        }
+        
+
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
